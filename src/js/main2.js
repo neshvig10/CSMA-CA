f7924a419820d647538a4025c1a5f240cd08ee31
Vigneshwaran <s.vigneshwaran.cse21@iitbhu.ac.in>
2023-06-01 14:28:25 +0530

    colors added to bot channel

--- src/js/main2.js
+++ src/js/main2.js
@@ -2,6 +2,7 @@ let traveltime=5000;
 let node1sendingframe=false;
 
 let time1="0";
+let time2="0";
 
 const display = document.getElementById("page1display");
 const cw1=document.getElementById("cw1");
@@ -76,6 +77,65 @@ function refreshTime() {
     cw5.style.backgroundColor="green";
     time1="2";
   }
+
+
+
+  if (time2==="0")
+  {
+    dw1.style.backgroundColor="green";
+    dw2.style.backgroundColor="green";
+    dw3.style.backgroundColor="green";
+    dw4.style.backgroundColor="green";
+    dw5.style.backgroundColor="green";
+  }
+
+  if (time2==="5")
+  {
+    dw1.style.backgroundColor="green";
+    dw2.style.backgroundColor="green";
+    dw3.style.backgroundColor="green";
+    dw4.style.backgroundColor="green";
+    dw5.style.backgroundColor="red";
+    time2="0";
+  }
+  if (time2==="4")
+  {
+    dw1.style.backgroundColor="green";
+    dw2.style.backgroundColor="green";
+    dw3.style.backgroundColor="green";
+    dw4.style.backgroundColor="red";
+    dw5.style.backgroundColor="green";
+    time2="5";
+  }
+  if (time2==="3")
+  {
+    dw1.style.backgroundColor="green";
+    dw2.style.backgroundColor="green";
+    dw3.style.backgroundColor="red";
+    dw4.style.backgroundColor="green";
+    dw5.style.backgroundColor="green";
+    time2="4";
+  }
+  if (time2==="2")
+  {
+    dw1.style.backgroundColor="green";
+    dw2.style.backgroundColor="red";
+    dw3.style.backgroundColor="green";
+    dw4.style.backgroundColor="green";
+    dw5.style.backgroundColor="green";
+    time2="3";
+  }
+  if (time2==="1")
+  {
+    dw1.style.backgroundColor="red";
+    dw2.style.backgroundColor="green";
+    dw3.style.backgroundColor="green";
+    dw4.style.backgroundColor="green";
+    dw5.style.backgroundColor="green";
+    time2="2";
+  }
+
+
 }
 setInterval(refreshTime, 1000);
 
@@ -119,7 +179,7 @@ function channelstatus(){
   channel_busy=false;
   console.log("Botsent");
 
-  rand=Math.random()*10;
+  rand=Math.random()*15;
   console.log(rand*1000);
 
   setTimeout(() => {
@@ -131,7 +191,7 @@ function channelstatus(){
 
 function botsendframe(){
   channel_busy=true;
-
+  time2="1";
   console.log("BotsendingFrame");
 
   setTimeout(() => {
